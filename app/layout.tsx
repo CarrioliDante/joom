a556c0a484ee147c78c915caafcbd740eaba0c62
CarrioliDante <carriolidante@gmail.com>
2024-08-11 01:05:29 -0300

    [ADD] Added clerk-auth and sidebar functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,10 +22,26 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${inter.className} bg-dark-2`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: '/icons/yoom-logo.svg',
+          socialButtonsVariant: 'iconButton',
+        },
+        variables: {
+          colorText: '#fff',
+          colorPrimary: '#0e78e9',
+          colorBackground: '#1c1f2e',
+          colorInputBackground: '#252a41',
+          colorInputText: '#fff',
+        },
+      }}
+    >
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './mobile-nav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
-    <nav className='flex-between bg-dark-1 fixed z-50 w-full px-6 py-4 lg:px-10'>
+    <nav className='flex-between bg-dark-1 fixed z-50 flex w-full px-6 py-4 lg:px-10'>
       <Link href='/' className='flex items-center gap-1'>
         <Image
           src='/icons/logo.svg'
@@ -18,8 +19,10 @@ const Navbar = () => {
           Joom
         </p>
       </Link>
-      <div className='flex-between gap-5'>
-        {/* Cler-user mngmnt */}
+      <div className='flex-between flex gap-5'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
         <MobileNav></MobileNav>
       </div>
